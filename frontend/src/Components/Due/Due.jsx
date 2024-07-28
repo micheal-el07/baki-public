598ccf94439eac91e0401d43e8be1e278888a452
micheal-el07 <mikhailkamal00@gmail.com>
2024-07-28 10:47:08 +0800

    change to use find method on due after and before due

--- frontend/src/Components/Due/Due.jsx
+++ frontend/src/Components/Due/Due.jsx
@@ -8,29 +8,27 @@ import Transaction from '../Transaction/Transaction'
 const Due = (props) => {
     const { recurring, allTransaction } = useContext(PageContext)
     const dateToday = Date
-    let due_bills = {after_due:[], before_due:[]}
+    let due_bills = { after_due: [], before_due: [] }
 
     for (let item_recurring of recurring) {
-        let is_paid = false;
-        for (let item_transaction of allTransaction) {
-            if (item_transaction.name === item_recurring.name) {
-                is_paid = true;
-            }
+        let is_billed = false
+        if (allTransaction.find((item) => item.name === item_recurring.name)) {
+            is_billed = true
         }
-        if (!is_paid) {
-            if (item_recurring.dayOfMonth >= dateToday.day) {
-                due_bills.before_due.push(item_recurring);
+        // Check if the items of recurring that have not yet paid is over the today day or not
+        if (!is_billed) {
+            if (item_recurring.dayOfMonth >= dateToday.day) { 
+                due_bills.before_due.push(item_recurring)
             } else {
-                due_bills.after_due.push(item_recurring);
+                due_bills.after_due.push(item_recurring)
             }
         }
     }
-    console.log(due_bills);
-    
+
     return (
         <div className='due'>
             {/* amount={total_due} */}
-            <TransactionHeader title={props.title}  />
+            <TransactionHeader title={props.title} />
             <div className="to-due">
                 <h3>To be Paid</h3>
                 {due_bills.before_due.map((item, i) => {
