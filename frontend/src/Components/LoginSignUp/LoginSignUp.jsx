67a567c62ffb1f70f6579db3b04c4d8eee6c9454
micheal-el07 <mikhailkamal00@gmail.com>
2024-07-31 13:35:16 +0800

    add loginsignup page

---
+++ frontend/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginSignUp = () => {
+    return (
+        <div className='loginsignup-container'>
+            <input type="text" name='username' />
+            <input type="text" name='name' />
+            <input type="password" name='password' />
+            <input type="password" name='re-password' />
+        </div>
+    )
+}
+
+export default LoginSignUp
